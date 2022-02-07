b682ecba1b15367bcbbcc02e7be76fc60607cb86
MaseDarHomeUbuntu <mailspecialforspam@mail.ru>
2022-02-07 22:27:43 +0300

    add react-router-dom

--- client/src/components/RoadMapList.tsx
+++ client/src/components/RoadMapList.tsx
@@ -1,30 +1,37 @@
-import React, {useEffect} from 'react';
-import {useTypedSelector} from "../hooks/useTypedSeletor";
-import {useActions} from "../hooks/useActions";
+import React, { useEffect } from "react";
+import { useTypedSelector } from "../hooks/useTypedSeletor";
+import { useActions } from "../hooks/useActions";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 const RoadMapList: React.FC = () => {
-    const { error, loading, roadmaps, limit} = useTypedSelector(state => state.roadmap)
-    const {getRoadmaps} = useActions()
-    const pages = [1, 2, 3, 4, 5]
+  const { error, loading, roadmaps, limit } = useTypedSelector(
+    (state) => state.roadmap
+  );
+  const { getRoadmaps } = useActions();
+  const pages = [1, 2, 3, 4, 5];
 
-    useEffect(() => {
-        getRoadmaps()
-    }, [])
+  useEffect(() => {
+    // getRoadmaps()
+  }, []);
 
-    if (loading) {
-        return <h1>Идет загрузка...</h1>
-    }
-    if (error) {
-        return <h1>{error}</h1>
-    }
+  if (loading) {
+    return <h1>Идет загрузка...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
 
-    return (
-        <div>
-            {roadmaps.map(roadmap =>
-                <div key={roadmap.id}>{roadmap.id} - {roadmap.name} - {roadmap.description}</div>
-            )}
+  return (
+    <div>
+      {roadmaps.map((roadmap) => (
+        <div key={roadmap.id}>
+          {roadmap.id} - {roadmap.name} - {roadmap.description}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RoadMapList;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./store";
+import ReactDOM from "react-dom";
+import RootComponent from "./RootComponent";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <RootComponent />
+  </Provider>,
+  document.getElementById("root")
 );
